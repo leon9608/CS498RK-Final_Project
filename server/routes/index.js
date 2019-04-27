c07fa855b10e68f536ab2ea1dfac82741d084bc9
Jiaxuan Guo <jiaxuan2@illinois.edu>
2019-04-27 16:02:43 -0500

    implement favorite & change the routes structure

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,6 @@
 
 module.exports = function (app, router) {
   app.use('/api', require('./home.js')(router));
-  app.use('/api/createUser',require('./createUser.js'));
-  app.use('/api/signIn',require('./signIn.js'));
+  app.use('/api/user',require('./user.js'));
   app.use('/api/posts', require('./posts.js'));
-  app.use('/api/postJob', require('./postJob.js'));
-  app.use('/api/deletePost', require('./deletePost.js'));
-  app.use('/api/getPostsByUser', require('./getPostsByUser.js'));
 };
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,26 +1,24 @@
 var express = require('express'),
     router = express.Router(),
     posts = require('../models/post'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    users = require('../models/user');
 
+// Respond with a list of posts
 // Get api/posts
 router.get('/', function(req, res){
-    //Default list
     var query = posts.find({});
 
-    //Search with where
     let whereClause = req.query.where;
     if(whereClause){
         query = posts.find(JSON.parse(whereClause));
     }
 
-    //Sort
     let sortClause = req.query.sort;
     if(sortClause){
         query = query.sort(JSON.parse(sortClause));
     }
 
-    //display only certain parameters
     query.
         select('jobName description contactName dateCreated').
         exec((err, res_posts) => {
@@ -38,7 +36,8 @@ router.get('/', function(req, res){
     });
 });
 
-//Get api/posts/:id
+// Response with a post detail by post id
+// Get api/posts/:id
 router.get("/:id", function(req, res){
     posts.findById(req.params.id, (err, res_post) => {
         if(err || !res_post) {
@@ -54,5 +53,61 @@ router.get("/:id", function(req, res){
         }
     });
 });
+
+// Create a new post in the database, professor only
+// Post api/posts/add
+router.post('/add', function(req, res){
+    var post = new posts(req.body);
+    var userId = req.body.userId;
+
+    post.
+        save().
+        then(res_post => {
+        users.findByIdAndUpdate(userId, {$push: {postsIds: res_post._id}}, function (err, item) {
+            if(err){
+                res.status(404).send({
+                    message: 'Failed to update the posts to the user',
+                    data: []
+                });
+            } else {
+                res.status(201).send({
+                    message: 'OK',
+                    data: res_post
+                });
+            }
+        })
+        }).
+        catch(err => {
+            console.log(err)
+            res.status(404).send({
+                message: 'Failed to create the job post',
+                data: []
+            });
+        });
+
+});
+
+// Delete specific post by post id, only for professor
+// Delete api/posts/delete/:id
+router.delete('/delete/:id', function(req, res){
+    posts.findByIdAndDelete(req.params.id, (err, res_post) => {
+        console.log(err)
+        console.log(res_post)
+        if(err || !res_post){
+            res.status(404).send({
+                message: 'Failed'
+            });
+        } else {
+            users.findOneAndUpdate({postsIds:res_post._id},{$pull: {postsIds: res_post._id}}, function(err, item){
+                console.log(err)
+                console.log(item)
+                res.status(200).send({
+                    message: 'Success'
+                });
+            })
+        }
+    });
+});
+
 
 module.exports = router;
